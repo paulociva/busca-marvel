ba2bf65b5727401d9aefd62817ab4a4e99c01c13
Paulo Eduardo Civa <paulociva@hotmail.com>
2020-07-06 22:49:42 -0300

    Remove unused test files

--- src/app/layout/pipes/array-filter-by-value.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import {ArrayFilterByValuePipe} from './array-filter-by-value.pipe';
-
-describe('ArrayFilterByValuePipe', () => {
-  it('create an instance', () => {
-    const pipe = new ArrayFilterByValuePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
